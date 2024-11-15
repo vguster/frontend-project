89792085d12c5d5ba9c918ab2890e787aceedc28
vguster <guster.victoria@johak.at>
2024-11-15 11:08:29 +0100

    added DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn">DaisyUI</button>
+
+<style>
+	.text {
+		color: turquoise;
+		font-size: 50px;
+	}
+</style>
